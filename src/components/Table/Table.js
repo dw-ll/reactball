0b4f64dd326bbacf41cfe5835ba1ee4cf75a9c94
Daniel Williams <danswilliams95@gmail.com>
2020-01-12 22:23:08 -0800

    Infinite table rendering.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,19 +3,27 @@ import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import Table from "react-bootstrap/Table";
 const DataTable = () => {
-  const playerList = useSelector(state => state.players);
+  const [completeData, setCompleteData] = React.useState([]);
+  const [currentData, setCurrentData] = React.useState({});
+  const players = useSelector(state => state.players);
+  var cData = {};
+  React.useEffect(players => {
+    if (!completeData) {
+    }
+  });
   const search = async val => {
     try {
       const res = await axios(
         `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${val}`
       );
-      const responseData = await res.data.data;
+      const responseData = res.data.data;
       console.log(responseData);
       return responseData;
     } catch (err) {
       console.log(err);
     }
   };
+
   return (
     <div>
       <h3>2019-2020 Season Averages</h3>
@@ -33,33 +41,30 @@ const DataTable = () => {
           </tr>
         </thead>
         <tbody>
-          {Object.keys(playerList).map(function(item, i) {
-            var currentPlayerData = search(playerList[item]);
-            console.log("Current Player Data:" + currentPlayerData);
-            if (currentPlayerData[0] !== undefined) {
+          {Object.keys(players).map(function(item, i) {
+            var currentPlayerData = search(players[item])
+              .then(result => {
+                cData = JSON.parse(JSON.stringify(result[0]));
+
+                // setCurrentData(cData);
+              })
+              .catch(err => {
+                console.log(err);
+              });
+            if (cData !== undefined) {
+              console.log("cData is here." + cData);
+              console.log(cData);
               return (
                 <tr key={i}>
                   <td>{item}</td>
-                  <td>{currentPlayerData[0].pts}</td>
-                  <td>{currentPlayerData[0]["ast"]}</td>
-                  <td>{currentPlayerData[0]["reb"]}</td>
-                  <td>{currentPlayerData[0]["stl"]}</td>
-                  <td>{currentPlayerData[0]["blk"]}</td>
-                  <td>{currentPlayerData[0]["turnover"]}</td>
+                  <td>{currentData.pts}</td>
+                  <td>{currentData.ast}</td>
+                  <td>{currentData.reb}</td>
+                  <td>{currentData.stl}</td>
+                  <td>{currentData.blk}</td>
+                  <td>{currentData.turnover}</td>
                 </tr>
               );
-            }else{return (
-                    <tr key={i}>
-                      <td>{item}</td>
-                      <td>0</td>
-                      <td>0</td>
-                      <td>0</td>
-                      <td>0</td>
-                      <td>0</td>
-                      <td>0</td>
-                    </tr>
-                  );
-
             }
           })}
         </tbody>
---
+++ src/reducers/counter.js
@@ -0,0 +1,12 @@
+/* eslint-disable default-case */
+const counter = (count = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return count + 1;
+    case "DECREMENT":
+      return count - 1;
+    default:
+      return count;
+  }
+};
+export default counter;
